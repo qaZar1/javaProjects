c72110aff838c39e3774f9ea6f35b9a71f9956da
Artem Efremov <qazar12321@yandex.ru>
2024-03-22 16:20:58 +0500

    [add] сделал первую таску

---
+++ L1.1 Simple web server/src/main/java/servlets/MirrorServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MirrorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String key = req.getParameter("key");
+
+        // Проверка наличия параметра key
+        if (key == null || key.isEmpty()) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameter 'key'");
+            return;
+        }
+
+        // Формирование ответа
+        String content = "<html><body>\n" +
+                "<h1>" + key + "</h1>\n" +
+                "</body></html>";
+
+        // Отправка ответа
+        resp.setContentType("text/html");
+        resp.getWriter().println(content);
+    }
+}
---
+++ L2.1 Authorization/src/main/java/accounts/AccountService.java
@@ -0,0 +1,60 @@
+package accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class AccountService {
+    private final Map<String, UserProfile> loginToProfile;
+    private final Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService() {
+        loginToProfile = new HashMap<>();
+        sessionIdToProfile = new HashMap<>();
+    }
+
+    public void addNewUser(UserProfile userProfile) {
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        return loginToProfile.get(login);
+    }
+
+    public UserProfile getUserBySessionId(String sessionId) {
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void addSession(String sessionId, UserProfile userProfile) {
+        sessionIdToProfile.put(sessionId, userProfile);
+    }
+
+    public void deleteSession(String sessionId) {
+        sessionIdToProfile.remove(sessionId);
+    }
+
+    private final Map<String, String> users = new HashMap<>();
+
+    public void addUser(String login, String password) {
+        users.put(login, password);
+    }
+
+    public boolean isRegistered(String login, String password) {
+        return users.containsKey(login) && users.get(login).equals(password);
+    }
+
+    private static AccountService instance;
+
+    public static AccountService getInstance() {
+        if (instance == null) {
+            instance = new AccountService();
+        }
+        return instance;
+    }
+}
