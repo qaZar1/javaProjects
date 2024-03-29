3c4a4e087fc861947bdda66d7e5186e1af696158
SavaLione <monologuesplus@gmail.com>
2024-03-29 18:23:08 +0500

    Update Main.java
    1. We can't use `allRequestsServlet.log` without proper initialization
    2. Because we can't use `allRequestsServlet.log` we want to print a string by using other function. `System.out.println` works fine
    3. We want to print a string before starting the web server.
    Even though we initialize server by `Server server = new Server(8080);` we start it by:
    ```java
            server.start(); // Starts server
            server.join(); // Waits when server will stop working
    ```
    Thus we want to put the line `System.out.println("Server started");` before `start` and `join`

--- L1.1 Simple web server/src/main/java/main/Main.java
+++ L1.1 Simple web server/src/main/java/main/Main.java
@@ -28,8 +28,9 @@ public class Main {
         Server server = new Server(8080);
         server.setHandler(context);
 
+        System.out.println("Server started");
+
         server.start();
         server.join();
-        allRequestsServlet.log("Server started");
     }
 }
